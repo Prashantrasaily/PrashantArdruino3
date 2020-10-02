601747300c7cdf4bbab70f506537faebf685117a
Sonia22545 <72243110+Sonia22545@users.noreply.github.com>
2020-10-02 15:32:07 +0530

    UPDATED THE COMMENTS(documentation)

--- Blinkme3.cpp
+++ Blinkme3.cpp
@@ -6,13 +6,13 @@ void setup() {
 //Serial.begin(9600);
 //pinMode(btnpin,INPUT_PULLUP);
 //pinMode(ledpin,OUTPUT);
-DDRD = B00100000; // sets the pin to input or output 
-PORTD = B00000100; // firstly checks the state of pin in DDRD register.
+DDRD = B00100000; // sets the pin to input or output (setting the DDRD register to 1 and 0 sets the pin to output and input respectively)
+PORTD = B00000100; // firstly checks the state of pin in DDRD register. (the value of this can be set in corrospondance to the DDRD register)
                    // in case of input enables or disables internal pullup register...in case of output sets the pin to high or low .
 }
 
 void loop() {
-  int btn = (_BV(btnpin)&PIND)>>2; // reading the state of btn pin 
+  int btn = (_BV(btnpin)&PIND)>>2; // reading the state of btn pin (_BV means 1<<)
   if(btn==HIGH){
     PORTD=_BV(ledpin)|PORTD;// turning led high and setting the bits 
   }
